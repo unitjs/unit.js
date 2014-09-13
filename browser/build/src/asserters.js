4d2308a3b43846b58aedb32af4e1217badfded0b
Nicolab.net <dev@nicolab.net>
2014-09-13 13:21:22 +0200

    [browser] Add browser support

---
+++ browser/build/src/asserters.js
@@ -0,0 +1,15 @@
+/**
+ * This file is part of the Unit.js testing framework.
+ *
+ * (c) Nicolas Tallefourtane <dev@nicolab.net>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file distributed with this source code
+ * or visit http://unitjs.com.
+ *
+ * @author Nicolas Tallefourtane <dev@nicolab.net>
+ */
+
+'use strict';
+
+// asserters are added below
